00e9d96099647038d79b324e7eb0e9661132c504
yasatria <satriamarfel@gmail.com>
2020-02-27 20:08:33 +0700

    parcelable

--- app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -1,15 +1,39 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import id.ac.polinema.intent.Model.User;
+
 public class ParcelableActivity extends AppCompatActivity {
+
+    public static final String  USER_KEY = "USER_KEY";
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
+    }
+
+    public void handleSubmit(View view) {
+        String username = usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
+        //User user = new User(username, name,age);
+        Intent intent = new Intent(this, ProfileParcelableActivity.class);
+        intent.putExtra(USER_KEY,new User(username, name,age));
+        startActivity(intent);
+
     }
 
 }
